56c3a6a09cd9bddba12422e6ecd4fbe0490e9630
ALEX <alex@ALEXs-MacBook-Pro.local>
2024-03-08 10:15:01 +0100

    Refactor AffaireForm state and add dynamic mode toggle
    - Refactored state management in AffaireForm.jsx, combining related states into a single state object for better organization and readability.
    - Introduced a new boolean state 'isMultipleMode' to toggle between Single and Multiple modes.
    - Implemented a dynamic button in AffaireForm to switch between Single and Multiple modes, enhancing user interactivity.
    - Updated the fetchCommunes and fetchDepartments functions to improve data handling and logging.
    - Added detailed comments for better code understanding and maintenance.
    - Improved form UI elements for consistency and responsiveness, including dynamic styling and hover effects for the submission button.

--- frontend/src/components/AffaireForm.jsx
+++ frontend/src/components/AffaireForm.jsx
@@ -2,13 +2,22 @@ import React, { useEffect, useState } from 'react';
 
 const apiAddress = "http://localhost:5000";
 
+
+const initialLocation = { department: '', commune: '', precision: '' };
 const AffaireForm = () => {
-    const [nomDeLaffaire, setNomDeLaffaire] = useState('');
-    const [department, setDepartment] = useState(''); // variable to store only the department choosen by the client 
-    const [departments, setDepartments] = useState([]); // variable to store the stire department list
-    const [commune, setCommune] = useState([]);
-    const [communes, setCommunes] = useState([]);
-    const [precision, setPrecision] = useState('');
+
+    const [location, setLocations] = useState([initialLocation]);
+    const [isMultipleMode, setIsMultipleMode] = useState(false);
+
+    const [nomDeLaffaire, setNomDeLaffaire] = useState(''); // title for the case (string)
+
+    const [department, setDepartment] = useState(''); // variable to store only the department choosen by the client  (string)
+    const [departments, setDepartments] = useState([]); // variable to store the entire department list fetch from the backend !array[]
+
+    const [commune, setCommune] = useState(''); // variable to store only the commune chosses by the client (string)
+    const [communes, setCommunes] = useState([]); // variable to store the entire commune list fetched form the backed 
+
+    const [precision, setPrecision] = useState(''); // case description
 
     /*
     
@@ -24,18 +33,27 @@ const AffaireForm = () => {
     const fetchDepartments = async () => {
         fetch(`${apiAddress}/departement`)
             .then(response => response.json())
-            .then(data => {setDepartments(data); console.log(departments)})
+            .then(data => {setDepartments(data); console.log(departments)}) // here we assign all the departments inside the useState variable departments
             .catch(error => console.error('Error fetching departements:', error));
     }
-
+    // ─────────────────────────────────────────────────────────────────────
+    
     const fetchCommunes = async () => {
         if (department) {
             fetch(`${apiAddress}/communes?dep_code=${department}`)
                 .then(response => response.json())
-                .then(data => { setCommunes(data); console.log(communes) })
+                .then(data => { setCommunes(data); console.log(communes[3]) })
                 .catch(error => console.error('Error fetching departements:', error));
         }
     }
+    // ─────────────────────────────────────────────────────────────────────
+
+    const toggleMode = (event) => {
+        event.preventDefault();
+        setIsMultipleMode(!isMultipleMode);
+    }
+
+    // ─────────────────────────────────────────────────────────────────────
 
     /*
     
@@ -58,6 +76,10 @@ const AffaireForm = () => {
     }, [department]);
     // ─────────────────────────────────────────────────────────────────────
 
+    // useEffect(() => {
+    //     ini
+    // }, [commune])
+
 
 
     /*
@@ -75,6 +97,15 @@ const AffaireForm = () => {
         <div className="flex justify-center items-center min-h-screen bg-gray-100">
             {/* Width full under small screen, max-width large under large screen, white background, shadow around form, rounded corners, padding, and margin bottom */}
             <form className="w-full max-w-lg bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+
+                <div >
+                    < button onClick={toggleMode} className={`${isMultipleMode ? 'bg-blue-500 mb-5 hover:bg-blue-700' : 'bg-green-500 mb-5 hover:bg-green-700'}
+                    text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:-translate-y-1 
+                    hover:scale-110`}>
+                        Switch to {isMultipleMode ? 'Single' : 'Multiple'} Mode
+                    </button>
+                </div>
+
                 {/* Margin bottom */}
                 <div className="mb-4">
                     {/* Block display, dark gray text, small text size, bold font, margin bottom */}
@@ -96,11 +127,12 @@ const AffaireForm = () => {
                         Département
                     </label>
                     <select id="department" value={department}
-                        onChange={(e) => setDepartment(e.target.value)}
+                        onChange={(e) => setDepartment(e.target.value)} // here we assign the department choosen by the user, that eventually it will be used to search the commune
                         className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-                        <option value="">Choisissez un département</option>
+                        <option value="">Choisissez un département</option> {/*  the option html element allow us to set each individual item in the selection list */}
                         {departments.map((dep) => (
-                            <option key={dep.DEP_CODE} value={dep.DEP_CODE}>{`${dep.DEP_NOM} (${dep.DEP_CODE})`}</option>
+                            <option key={dep.DEP_CODE} value={dep.DEP_CODE}>{`${dep.DEP_NOM} (${dep.DEP_CODE})`}</option> /* in the deparments useState variable array we have all departments, 
+                                                                                                                             we will put them in the list programically with the map method  */
                         ))}
                     </select>
                 </div>
@@ -135,7 +167,9 @@ const AffaireForm = () => {
             {/* // ───────────────────────────────────────────────────────────────────────────── */}
 
                 {/* Button with background color blue, on hover darker blue, white text, bold, padding, rounded corners, focus outline none, focus shadow outline */}
-                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded 
+                focus:outline-none focus:shadow-outline transition duration-300 ease-in-out transform 
+                hover:-translate-y-1 hover:scale-110 ">
                     Submit
                 </button>
             </form>
